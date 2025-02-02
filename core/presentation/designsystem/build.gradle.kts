981eaa8cace72d57b9814dfc2a36e7e3f8762caa
angeladimovska <angela.dimovska@echo.com.mk>
2025-02-03 00:45:14 +0100

    add initial dependencies

--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -33,11 +33,13 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.material3)
 }
