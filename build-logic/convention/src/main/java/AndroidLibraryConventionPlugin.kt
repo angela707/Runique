54fd7b2484d77519ca42dfe8a2660551f9c5814f
angeladimovska <angela.dimovska@echo.com.mk>
2025-02-04 01:15:03 +0100

    Create the AndroidLibraryConventionPlugin. Remove unnecessary code in multiple build gradles.

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,40 @@
+import com.adimovska.convention.ExtensionType
+import com.adimovska.convention.configureBuildTypes
+import com.adimovska.convention.configureKotlinAndroid
+import com.adimovska.convention.getPluginId
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply(getPluginId("android-library"))
+                apply(getPluginId("kotlin-android"))
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.LIBRARY
+                )
+
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+
+            dependencies {
+                "testImplementation"(kotlin("test"))
+            }
+        }
+    }
+}
