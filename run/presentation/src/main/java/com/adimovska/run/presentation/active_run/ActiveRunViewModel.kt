bc2221b7127c13561b873ba127254dc262a56377
angeladimovska <angela.dimovska@echo.com.mk>
2025-02-09 18:28:10 +0100

    Listen for changes of the tracking, rundata, elapsedtime and current location in the view model.

--- run/presentation/src/main/java/com/adimovska/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/adimovska/run/presentation/active_run/ActiveRunViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.viewModelScope
 import com.adimovska.run.domain.RunningTracker
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
-import timber.log.Timber
 
 class ActiveRunViewModel(
     private val runningTracker: RunningTracker
@@ -22,10 +25,69 @@ class ActiveRunViewModel(
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
+
     private val hasLocationPermission = MutableStateFlow(false)
+
+    private val shouldTrack = state.map { it.shouldTrack }
+        .stateIn(viewModelScope, SharingStarted.Lazily, _state.value.shouldTrack)
+
+    val isTracking = combine(
+        shouldTrack,
+        hasLocationPermission
+    ) { shouldTrack, hasPermission ->
+        shouldTrack && hasPermission
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
 
 
     init {
+        listenForLocationPermissions()
+        listenForChangesInTracking()
+        listenForCurrentLocation()
+        listenForRunData()
+        listenForElapsedTime()
+    }
+
+    private fun listenForElapsedTime() {
+        runningTracker
+            .elapsedTime
+            .onEach { elapsedTime ->
+                _state.update {
+                    it.copy(elapsedTime = elapsedTime)
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun listenForRunData() {
+        runningTracker
+            .runData
+            .onEach { runData ->
+                _state.update { it.copy(runData = runData) }
+            }
+            .launchIn(viewModelScope)
+
+    }
+
+    private fun listenForCurrentLocation() {
+        runningTracker
+            .currentLocation
+            .onEach { currentLocation ->
+                _state.update {
+                    it.copy(currentLocation = currentLocation?.location)
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun listenForChangesInTracking() {
+        isTracking
+            .onEach { isTracking ->
+                runningTracker.setIsTracking(isTracking)
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun listenForLocationPermissions() {
         hasLocationPermission
             .onEach { hasPermission ->
                 if (hasPermission) {
@@ -35,20 +97,15 @@ class ActiveRunViewModel(
                 }
             }
             .launchIn(viewModelScope)
-
-        runningTracker
-            .currentLocation
-            .onEach { location ->
-                Timber.d("=+= New location $location ")
-            }
-            .launchIn(viewModelScope)
     }
 
     fun onAction(action: ActiveRunAction) {
 
         when (action) {
             ActiveRunAction.OnBackClick -> {
-
+                _state.update {
+                    it.copy(shouldTrack = false)
+                }
             }
 
             ActiveRunAction.OnFinishRunClick -> {
@@ -56,11 +113,18 @@ class ActiveRunViewModel(
             }
 
             ActiveRunAction.OnResumeRunClick -> {
-
+                _state.update {
+                    it.copy(shouldTrack = true)
+                }
             }
 
             ActiveRunAction.OnToggleRunClick -> {
-
+                _state.update {
+                    it.copy(
+                        hasStartedRunning = true,
+                        shouldTrack = !state.value.shouldTrack
+                    )
+                }
             }
 
             is ActiveRunAction.SubmitLocationPermissionInfo -> {
